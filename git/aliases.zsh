241bcab4ca437599a1548bed9f7d98a0924263e3
Geoff Whatley <me@geoffwhatley.com>
2017-07-18 09:13:02 +1000

    revert to plain `git commit` :zzz:

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,6 +1,6 @@
 alias g='git'
 alias ga='git add .'
-alias gc='git cz'
+alias gc='git commit'
 alias gca='git commit -a'
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
