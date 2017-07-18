7be9478fd3ebe922be423ed674946481d5ef4f38
Geoff Whatley <me@geoffwhatley.com>
2017-07-18 11:51:09 +1000

    fix(path): remove broken rvm package & redundant yarn path

---
+++ ruby/path.zsh
@@ -0,0 +1,7 @@
+
+export PATH="$PATH:$HOME/.rvm/bin"
+
+if [[ -s "$HOME/.rvm/scripts/rvm" ]]
+then
+  source "$HOME/.rvm/scripts/rvm"
+fi
--- yarn/path.zsh
+++
@@ -1,4 +0,0 @@
-if ! [[ -x "$(command -v yarn >/dev/null 2>&1)" ]]
-then
-  export PATH="$PATH:`yarn global bin`"
-fi
