0c089913175d804d88e3c1b985125de59b3a1d04
Geoff Whatley <me@geoffwhatley.com>
2017-07-19 11:01:31 +1000

    feat(git): add fetch & new branch alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,6 +3,8 @@ alias ga='git add .'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
+alias gf='git fetch'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gnb='git checkout -b'
 alias gp='git push origin HEAD'
 alias gs='git status -sb'
