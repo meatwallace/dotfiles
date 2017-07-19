1e6fc7f8c1f119a40a5d34362addb715f988c355
Geoff Whatley <me@geoffwhatley.com>
2017-07-19 11:02:11 +1000

    feat(vscode): symlink settings :tada:

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,5 +1,6 @@
-#!/bin/bash -e
+#!/bin/bash
 
+# install
 if [[ "$(uname -s)" == "Darwin" ]] && [[ ! -x "/Applications/Visual Studio Code - Insiders.app" ]]
 then
   brew cask install visual-studio-code-insiders
@@ -12,3 +13,23 @@ then
 
   sudo aptitude install code-insiders
 fi
+
+# set installation directory
+if [[ "$(uname -s)" == "Darwin" ]]
+then
+  VSCODE="${HOME}"/Library/Application\ Support/Code\ -\ Insiders/User
+elif [[ "$(uname -s)" == "Linux" ]]
+then
+  VSCODE="${HOME}"/.config/Code\ -\ Insiders/User
+fi
+
+# dotfiles
+rm -f "${VSCODE}/projects.json"
+rm -f "${VSCODE}/settings.json"
+rm -f "${VSCODE}/keybindings.json"
+rm -rf "${VSCODE}/snippets"
+
+ln -s "${ZSH}/vscode/projects.json" "${VSCODE}/projects.json"
+ln -s "${ZSH}/vscode/settings.json" "${VSCODE}/settings.json"
+ln -s "${ZSH}/vscode/keybindings.json" "${VSCODE}/keybindings.json"
+ln -s "${ZSH}/vscode/snippets" "${VSCODE}/snippets"
