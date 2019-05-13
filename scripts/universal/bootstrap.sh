f54b3cd56b0e86faaa843868e304fc0717d50768
Geoff Whatley <me@geoffwhatley.com>
2019-05-13 20:22:04 +1000

    refactor(#29): remove usage of bash-oo framework
    - removed usage of bash infinity in `meatbox` and it's installation in
    our bootstrap scripts (closes #29)
    - added placeholder single letter aliases and renamed tizonia aliases
    for clarity
    - reworked `meatbox` and `lowercase` functions to be posix compliant
    - reworked `lowercase` function to accept usage via stdin and arguments
    - created new `uppercase` function based on `lowercase`

--- scripts/universal/bootstrap.sh
+++ scripts/universal/bootstrap.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 declare -a scripts=(
   install_or_update_is.sh
-  install_or_update_bash_oo.sh
   install_or_update_app_fast.sh
   install_or_update_asdf.sh
   install_or_update_wd.sh
