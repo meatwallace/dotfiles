d6710918014235e53d172a37174d6b6cf429cb62
Geoff Whatley <me@geoffwhatley.com>
2019-05-15 04:24:25 +1000

    feat(#41): scaffold an alpine variant of our linux environment
    - added an Alpine Linux Dockerfile that installs packages listed in a
      file loaded via build context, finding a balance somewhere between our
      full Arch environment yet cutting cruft to keep it slim
    - restructured scripts directory to begin splitting scripts between
      distros
    - updated existing x11docker script and added one for Alpine, along with
      simpler run-tty scripts
    
    closes #41

---
+++ scripts/linux/arch/run-x11.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+x11docker \
+  --name=meatbox \
+  --desktop \
+  --hostdisplay \
+  --xorg \
+  --gpu \
+  --init=systemd \
+  --runtime=nvidia \
+  --dbus-system \
+  --clipboard \
+  --pulseaudio \
+  --cap-default \
+  --user=RETAIN \
+  --sudouser \
+  -- \
+  --cap-add=SYS_ADMIN \
+  -- \
+  meatwallace/meatbox-arch:latest /usr/bin/xinitrcsession-helper
+  # --fullscreen \
+  # --size=1840x1035 \
+  # --xephyr \
+  # --init=openrc \
