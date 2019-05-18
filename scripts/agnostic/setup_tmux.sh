552c3e53cc88dbff57e2770ad0f2f6136b476f79
Geoff Whatley <me@geoffwhatley.com>
2019-05-18 19:59:50 +1000

    feat(#66): add basic tmux configuration
    closes #66

--- scripts/agnostic/setup_tmux.sh
+++ scripts/agnostic/setup_tmux.sh
@@ -2,5 +2,12 @@
 
 set -eu
 
-git-clone-or-update https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
+git-clone-or-update https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm >/dev/null
+git-clone-or-update https://github.com/tmux-plugins/tmux-sensible ~/.tmux/plugins/tmux-sensible >/dev/null
+git-clone-or-update https://github.com/tmux-plugins/tmux-pain-control ~/.tmux/plugins/tmux-pain-control >/dev/null
+git-clone-or-update https://github.com/christoomey/vim-tmux-navigator ~/.tmux/plugins/vim-tmux-navigator >/dev/null
+git-clone-or-update https://github.com/tmux-plugins/tmux-copycat ~/.tmux/plugins/tmux-copycat >/dev/null
+git-clone-or-update https://github.com/tmux-plugins/tmux-open ~/.tmux/plugins/tmux-open >/dev/null
+git-clone-or-update https://github.com/tmux-plugins/tmux-yank ~/.tmux/plugins/tmux-yank >/dev/null
+git-clone-or-update https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect >/dev/null
+git-clone-or-update https://github.com/tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum >/dev/null
