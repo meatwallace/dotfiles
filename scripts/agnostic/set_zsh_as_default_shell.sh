a30b468af0c0992f2ef2a190cce81eb083db2df0
Geoff Whatley <me@geoffwhatley.com>
2019-05-23 15:34:36 +1000

    feat(#50): add zsh completion & vcs status packages to alpine image
    closes #50
    closes #84

--- scripts/agnostic/set_zsh_as_default_shell.sh
+++ scripts/agnostic/set_zsh_as_default_shell.sh
@@ -2,13 +2,12 @@
 
 set -eu
 
-user="$(id -u -n)"
+if ! grep "$USER" </etc/passwd | grep "zsh"; then
+  zsh_bin="$(grep --max-count=1 zsh </etc/shells)"
 
-# set ZSH as our default shell
-if grep "$user" </etc/passwd | grep "zsh"; then
   if [ -z "$MEATBOX_PASSWORD" ]; then
-    chsh -s /usr/bin/zsh
+    chsh -s "$zsh_bin"
   else
-    echo "$MEATBOX_PASSWORD" | chsh -s /usr/bin/zsh
+    echo "$MEATBOX_PASSWORD" | chsh -s "$zsh_bin"
   fi
 fi
--- scripts/alpine/install_or_update_packages.sh
+++ scripts/alpine/install_or_update_packages.sh
@@ -5,4 +5,4 @@ set -eu
 packages="$(grep -v '^\#' ./APKFile | grep .)"
 
 # shellcheck disable=SC2086
-sudo apk add --no-cache $packages >/dev/null 2>&1
+sudo apk add --no-cache $packages #>/dev/null 2>&1
