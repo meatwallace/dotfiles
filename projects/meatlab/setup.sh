1db865a8078f1ce9c69bd7bcf05e890fad151310
Geoff Whatley <me@geoffwhatley.com>
2019-06-20 17:15:11 +1000

    chore(#117): extract meatlab out of the main repository
    closes #117

--- projects/meatlab/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-mkdir -p "$HOME"/containers/heimdall/config
-mkdir -p "$HOME"/containers/plex/{config,transcode}
-mkdir -p "$HOME"/containers/downloads/completed/{movies,tv}
-
-(cd "$MEATLAB_DIR" && docker-compose up -d)
