802e6526630a8159bfab7c236017d723324b53d2
Geoff Whatley <me@geoffwhatley.com>
2019-08-25 13:06:08 +1000

    feat(tmux): swap tmux-copycat to tmux-fzf-url plugin

--- scripts/agnostic/setup_tmux.sh
+++ scripts/agnostic/setup_tmux.sh
@@ -9,11 +9,11 @@ setup_tmux() {
   git-clone-or-update https://github.com/tmux-plugins/tmux-sensible ~/.tmux/plugins/tmux-sensible >/dev/null
   git-clone-or-update https://github.com/tmux-plugins/tmux-pain-control ~/.tmux/plugins/tmux-pain-control >/dev/null
   git-clone-or-update https://github.com/christoomey/vim-tmux-navigator ~/.tmux/plugins/vim-tmux-navigator >/dev/null
-  git-clone-or-update https://github.com/tmux-plugins/tmux-copycat ~/.tmux/plugins/tmux-copycat >/dev/null
   git-clone-or-update https://github.com/tmux-plugins/tmux-open ~/.tmux/plugins/tmux-open >/dev/null
   git-clone-or-update https://github.com/tmux-plugins/tmux-yank ~/.tmux/plugins/tmux-yank >/dev/null
   git-clone-or-update https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect >/dev/null
   git-clone-or-update https://github.com/tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum >/dev/null
+  git-clone-or-update https://github.com/wfxr/tmux-fzf-url ~/.tmux/plugins/tmux-fzf-url >/dev/null
 }
 
 setup_tmux "$@"
