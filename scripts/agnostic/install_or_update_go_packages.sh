b5cea8d5e53b5636b50a2a49dd3e290e1b95ce52
Geoff Whatley <me@geoffwhatley.com>
2019-08-26 13:54:51 +1000

    fix(go): temporarily shift gitconfig to allow go get with github

--- scripts/agnostic/install_or_update_go_packages.sh
+++ scripts/agnostic/install_or_update_go_packages.sh
@@ -2,11 +2,24 @@
 
 set -eu
 
+trap revert_gitconfig 1 2 3 6
+
+revert_gitconfig() {
+  mv "$HOME/.gitconfig.bak" "$HOME/.gitconfig"
+}
+
 install_or_update_go_packages() {
   echo "go: installing packages"
 
+  # TODO: remove this hacky workaround. as we force git to use SSL for github
+  # URLs, it inteferes with `go get` prior to our SSL certificate being
+  # installed, so we temporarily shift our config file and then shift it back
+  mv "$HOME/.gitconfig" "$HOME/.gitconfig.bak"
+
   go get github.com/mattn/efm-langserver/cmd/efm-langserver
   go get golang.org/x/tools/cmd/gopls
+
+  revert_gitconfig
 }
 
 install_or_update_go_packages "$@"
