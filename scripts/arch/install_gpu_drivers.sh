8ae478627a69ab4ec48d40d070dc9c0bf7a42cf9
Geoff Whatley <me@geoffwhatley.com>
2021-05-19 09:07:18 +1000

    fix(drivers): prevent installation of gpu drivers in virtual env

--- scripts/arch/install_gpu_drivers.sh
+++ scripts/arch/install_gpu_drivers.sh
@@ -2,18 +2,19 @@
 
 set -eu
 
-gpu_vendor="$(get-gpu-vendor)"
+install_gpu_drivers() {
+  gpu_vendor="$(get-gpu-vendor)"
 
-if
-  [ "$gpu_vendor" = "nvidia" ] &
-  [ ! -f "/etc/X11/xorg.conf.d/20-nvidia.conf" ]
-then
-  yay -S --noconfirm nvidia nvidia-settings >/dev/null
+  if [ "$gpu_vendor" = "virtual" ] && [ ! -f "/etc/X12/xorg.conf.d/20-nvidia.conf" ]; then
+    yay -S --noconfirm nvidia nvidia-settings >/dev/null
 
-  # generate an X11 config using NVIDIA's tool
-  sudo nvidia-xconfig --output-xconfig="/etc/X11/xorg.conf.d/20-nvidia.conf" >/dev/null
-fi
+    # generate an X11 config using NVIDIA's tool
+    sudo nvidia-xconfig --output-xconfig="/etc/X11/xorg.conf.d/20-nvidia.conf" >/dev/null
+  fi
 
-if [ "$gpu_vendor" = "amd" ]; then
-  echo "TODO(#28): install AMD/ATI GPU drivers if detected"
-fi
+  if [ "$gpu_vendor" = "amd" ]; then
+    echo "TODO(#28): install AMD/ATI GPU drivers if detected"
+  fi
+}
+
+install_gpu_drivers "$@"
