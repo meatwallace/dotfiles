0a232afa6b20b133694bdc95852553d07795b58c
Geoff Whatley <me@geoffwhatley.com>
2021-05-19 13:07:00 +1000

    fix(zsh): update zplugin references to zinit

--- scripts/agnostic/update.sh
+++ scripts/agnostic/update.sh
@@ -9,7 +9,7 @@ install_or_update_node_modules.sh
 install_or_update_reason_language_server.sh
 install_or_update_circleci_cli.sh
 install_or_update_cheatsh.sh
-install_or_update_zplugin.sh
+install_or_update_zinit.sh
 setup_tmux.sh
 setup_nvim.sh
 set_zsh_as_default_shell.sh
