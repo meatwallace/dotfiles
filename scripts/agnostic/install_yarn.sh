81a7bbf7693eac8ba793bc19372e42a0e571ad0b
Geoff Whatley <me@geoffwhatley.com>
2021-05-19 13:09:28 +1000

    fix(yarn): update yarn installation and usage for v1 compat

--- scripts/agnostic/install_yarn.sh
+++ scripts/agnostic/install_yarn.sh
@@ -4,11 +4,8 @@ set -eu
 
 install_yarn() {
   if ! command -v yarn; then
-    if sysinfo distro | grep -q "alpine"; then
-      sudo apk add --no-cache yarn
-    else
-      sudo pacman -Sy --noconfirm --config="$HOME/.config/pacman/pacman.conf" yarn
-    fi
+    cd "$HOME" && \
+    npm install -g yarn
   fi
 }
 
