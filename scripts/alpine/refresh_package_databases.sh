c91630ba3f9ea4982f9213c1f88bca172149b510
Geoff Whatley <me@geoffwhatley.com>
2021-12-01 23:09:43 +1100

    chore(#143): remove alpine linux config
    closes #143

--- scripts/alpine/refresh_package_databases.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-refresh_package_databases() {
-  sudo apk update
-}
-
-refresh_package_databases
--- scripts/alpine/update_openrc_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-# make sure our terminal will support unicode
-if ! sudo cat /etc/rc.conf | grep -q 'unicode="YES"'; then
-  echo 'unicode="YES"' | sudo tee -a /etc/rc.conf
-fi
-
