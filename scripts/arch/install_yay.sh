209d6bef172654c10ce5d2108c0e7636ef93e63e
Geoff Whatley <me@geoffwhatley.com>
2021-12-02 09:12:23 +1100

    fix(scripts): manually build yay from aur source

--- scripts/arch/install_yay.sh
+++ scripts/arch/install_yay.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-set -eu
+set -u
+
+tempdir="$(mktemp -d .yay.XXXXXXXX)"
+
+cleanup() {
+  rm -rf "$tempdir"
+}
+
+trap cleanup 0
 
 install_yay() {
   # install `yay` if it's unavalable, a `pacman` wrapper that integrates the AUR
   if [ ! -x "$(command -v yay)" ]; then
-    sudo pacman -S --noconfirm --config="$HOME/.config/pacman/pacman.conf" yay
+    git clone https://aur.archlinux.org/yay.git "$tempdir" >/dev/null
+    cd "$HOME/.yay"
+    makepkg -si --noconfirm
+    cleanup
   fi
 }
 
